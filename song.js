4e9efc306105a9382fcab6ae390d5a9a8460956a
nexx <129945000+nexxing@users.noreply.github.com>
2025-01-20 11:42:27 -0500

    yuh

--- song.js
+++ song.js
@@ -7,3 +7,28 @@ var sound = new Howl({
 function playSound() {
     sound.play();
 }
+
+// Play sound immediately on page load
+playSound();
+
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	setInterval(loop, 350);
+}
+
+var x = 0;
+
+var titleText = [ "🖤 xan", "🖤 xa", "🖤 x", "🖤", "🖤 x", "🖤 xa", "🖤 xan"];
+
+function loop(){
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+}
